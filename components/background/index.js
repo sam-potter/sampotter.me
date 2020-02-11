85ccdb23829c14e3e8ddf358dcb02b4d6813b546
Sam Potter <sam.potter@hotmail.com>
2020-02-10 17:18:56 -1000

    Added face model using OrbitControls

--- components/background/index.js
+++ components/background/index.js
@@ -1,69 +1,56 @@
-import * as THREE from "three";
-import React, { useEffect, useState, useRef } from "react";
-import { Canvas, useFrame } from "react-three-fiber";
+import React, { Suspense, useRef, useEffect } from "react";
+import { GLTFLoader } from "./GLTFLoader";
+import { OrbitControls } from "./OrbitControls";
+import {
+  extend,
+  Canvas,
+  useLoader,
+  useThree,
+  useFrame
+} from "react-three-fiber";
 import classes from "./background.module.css";
 
-function Box(props) {
-  return (
-    <mesh {...props}>
-      <boxBufferGeometry attach="geometry" args={[1, 1, 1, 1]} />
-      <meshBasicMaterial attach="material" map={props.texture} />
-    </mesh>
-  );
+extend({ OrbitControls });
+
+function Head() {
+  const gltf = useLoader(GLTFLoader, "/static/three/modelSD.glb");
+  return <primitive object={gltf.scene} position={[0, 0, 0]} />;
 }
 
-function Background(props) {
-  let easing = 0.00005;
-  let mouseX = 0;
-  let mouseY = 0;
+function Controls(props) {
+  const ref = useRef();
+  const {
+    camera,
+    gl: { domElement }
+  } = useThree();
+  useFrame(() => ref.current.update());
+  return <orbitControls ref={ref} args={[camera, domElement]} {...props} />;
+}
+
+function Background() {
   let container = useRef();
-  let boxes = Array(1000).fill(0);
-  let [texture, setTexture] = useState();
+  let options = {
+    enableZoom: false,
+    enableDamping: true,
+    dampingFactor: 1.5,
+    rotateSpeed: 0.2,
+    maxPolarAngle: 45,
+    minPolarAngle: 0,
+    minAzimuthAngle: -Math.PI * (1 / 2),
+    maxAzimuthAngle: Math.PI * (1 / 2)
+  };
 
-  useEffect(() => {
-    setTexture(new THREE.TextureLoader().load("/static/images/texture.jpg"));
-    // eslint-disable-next-line
-    gsap.to(container.current, 500, { opacity: 100 });
-  }, []);
-
-  function random(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) + min);
-  }
-
-  function _handleDocumentMouseMove(event) {
-    mouseX = (event.clientX - window.innerWidth / 2) * 2;
-    mouseY = (event.clientY - window.innerHeight / 2) * 2;
-  }
-
-  function Camera() {
-    function map(n, start1, stop1, start2, stop2) {
-      return ((n - start1) / (stop1 - start1)) * (stop2 - start2) + start2;
-    }
-
-    useFrame(({ camera }) => {
-      let _easing = map(Math.abs(camera.position.x), 0, 10, easing, 0);
-
-      camera.position.x += (mouseX - camera.position.x) * _easing;
-      camera.position.y += (-mouseY - camera.position.y) * _easing;
-
-      camera.lookAt(0, 0, 0);
-      camera.updateProjectionMatrix();
-    });
-
-    return null;
-  }
+  // eslint-disable-next-line
+  useEffect(_ => gsap.to(container.current, 0.5, { opacity: 1 }));
 
   return (
     <div className={classes.Container} ref={container}>
-      <Canvas position={[0, 0, 600]} onPointerMove={_handleDocumentMouseMove}>
+      <Canvas camera={{ position: [0, 0, 300] }}>
         <ambientLight />
-        {boxes.map((el, i) => {
-          let x = random(-20, 20);
-          let y = random(-20, 20);
-          let z = random(-20, 20);
-          return <Box key={i} texture={texture} position={[x, y, z]} />;
-        })}
-        <Camera />
+        <Controls {...options} />
+        <Suspense fallback={null}>
+          <Head />
+        </Suspense>
       </Canvas>
     </div>
   );
