b518983ea1f124e44bc8c5287d6848d3e7f80357
Sam Potter <sam.potter@hotmail.com>
2020-02-10 18:28:56 -1000

    updated charSet bug

--- components/seo/index.js
+++ components/seo/index.js
@@ -7,7 +7,7 @@ const SEO = ({ title }) => (
     <script src="/static/scroll.js" />
     <script src="/static/gsap.min.js/" />
 
-    <meta charset="utf-8" />
+    <meta charSet="utf-8" />
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <meta name="author" content="Sam Potter" />
