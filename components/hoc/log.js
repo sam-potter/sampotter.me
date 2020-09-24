cc1b203597496c5d8d356541f96fb1614e046fbb
Sam Potter <sam.potter@hotmail.com>
2020-09-24 16:24:53 +1000

    v1

--- components/hoc/log.js
+++
@@ -1,6 +0,0 @@
-const log = (Comp) => (props) => {
-  console.log(`Rendering ${Comp.name}`);
-  return <Comp {...props} />;
-};
-
-export default log;
--- components/hoc/pure.js
+++
@@ -1,9 +0,0 @@
-import { PureComponent } from 'react';
-
-export default function(Comp) {
-  return class extends PureComponent {
-    render() {
-      return <Comp {...this.props} />;
-    }
-  };
-}
