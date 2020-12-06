2867e62ab87ecfdf873cc7f5a65be20347f1769d
Sam Potter <sam.potter@hotmail.com>
2020-12-06 20:43:55 +1100

    Removed email button

--- src/components/footer.js
+++ src/components/footer.js
@@ -15,7 +15,6 @@ export default function Footer() {
       <p className="text-gray-400">Get in touch →</p>
       <div className="space-x-4">
         <Link href="https://github.com/saampotter">GitHub</Link>
-        <Link href="mailto:sam.potter@hotmail.com">Email</Link>
         <Link href="https://twitter.com/saampotter_">Twitter</Link>
         <Link href="https://medium.com/@sampotter_">Medium</Link>
       </div>
