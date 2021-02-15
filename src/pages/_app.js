46954e41cff4e31dfe998ca00d59bce3378adab5
Sam Potter <sam.potter@hotmail.com>
2021-02-15 13:28:11 +1100

    Added classnames for purgecss to pickup

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -4,6 +4,20 @@ import { DefaultSeo } from 'next-seo';
 import 'tailwindcss/tailwind.css';
 import seoConfig from '../../seo.config';
 
+// const tokenClassNames = {
+//   tag: 'text-code-red',
+//   'attr-name': 'text-code-yellow',
+//   'attr-value': 'text-code-green',
+//   deleted: 'text-code-red',
+//   inserted: 'text-code-green',
+//   punctuation: 'text-code-white',
+//   keyword: 'text-code-purple',
+//   string: 'text-code-green',
+//   function: 'text-code-blue',
+//   boolean: 'text-code-red',
+//   comment: 'text-gray-400 italic',
+// };
+
 const App = ({ Component, pageProps }) => (
   <>
     <Head>
