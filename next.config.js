f64dea1be76fb6a3a743ec87cd867622186ef3e6
Sam Potter <sam.potter@hotmail.com>
2021-02-15 11:48:59 +1100

    Added blog + first article

--- next.config.js
+++ next.config.js
@@ -1,4 +1,23 @@
+const visit = require('unist-util-visit');
+const rehypePrism = require('@mapbox/rehype-prism');
+const { createLoader } = require('simple-functional-loader');
+
+const tokenClassNames = {
+  tag: 'text-code-red',
+  'attr-name': 'text-code-yellow',
+  'attr-value': 'text-code-green',
+  deleted: 'text-code-red',
+  inserted: 'text-code-green',
+  punctuation: 'text-code-white',
+  keyword: 'text-code-purple',
+  string: 'text-code-green',
+  function: 'text-code-blue',
+  boolean: 'text-code-red',
+  comment: 'text-gray-400 italic',
+};
+
 module.exports = {
+  pageExtensions: ['js', 'jsx', 'mdx'],
   async rewrites() {
     return [
       {
@@ -11,9 +30,48 @@ module.exports = {
       },
     ];
   },
-  webpack: (config, { dev, isServer }) => {
+  webpack: (config, options) => {
+    const mdx = [
+      options.defaultLoaders.babel,
+      {
+        loader: '@mdx-js/loader',
+        options: {
+          rehypePlugins: [
+            rehypePrism,
+            () => {
+              return tree => {
+                visit(tree, 'element', (node, index, parent) => {
+                  let [token, type] = node.properties.className || [];
+                  if (token === 'token') {
+                    node.properties.className = [tokenClassNames[type]];
+                  }
+                });
+              };
+            },
+          ],
+        },
+      },
+    ];
+
+    config.module.rules.push({
+      test: /\.mdx$/,
+      use: [
+        ...mdx,
+        createLoader(function (src) {
+          const content = [
+            'import Post from "@/components/post"',
+            '// export { getStaticProps } from "@/getStaticProps"',
+            src,
+            'export default Post',
+          ].join('\n');
+
+          return this.callback(null, content);
+        }),
+      ],
+    });
+
     // Replace React with Preact only in client production build
-    if (!dev && !isServer) {
+    if (!options.dev && !options.isServer) {
       Object.assign(config.resolve.alias, {
         react: 'preact/compat',
         'react-dom/test-utils': 'preact/test-utils',
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,4 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
+const isDev = process.env.NODE_ENV === 'development';
 
 export default class NextSite extends Document {
   render() {
@@ -15,17 +16,11 @@ export default class NextSite extends Document {
           <meta name="language" content="en" />
           <meta name="author" content="Sam Potter" />
           <meta name="robots" content="follow, all" />
-          <meta name="theme-color" content="#ffffff" />
-          <link rel="manifest" href="/manifest.json" />
-          <link rel="apple-touch-icon" sizes="192x192" href="/apple-touch-icon.png" />
-          <link rel="shortcut icon" sizes="32x32" href="/favicon-32x32.png" />
-          <link rel="shortcut icon" sizes="16x16" href="/favicon-16x16.png" />
-          <link rel="shortcut icon" sizes="192x192" href="/android-icon-192x192.png" />
         </Head>
-        <body className="bg-white dark:bg-black">
+        <body className="bg-white">
           <Main />
           <NextScript />
-          <script async data-api="/_hive" src="/bee.js"></script>
+          {!isDev && <script async data-api="/_hive" src="/bee.js"></script>}
         </body>
       </Html>
     );
